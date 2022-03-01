3f8fd639c05e98cab5e4190dc72442f50fdf6229
Davis <obviouspy@gmail.com>
2022-03-01 11:19:10 -0500

    Reintroduce packet.zig file

---
+++ src/client/net/packet.zig
@@ -0,0 +1,16 @@
+const User = @import("../users.zig").User;
+
+/// A user with its ID, for use in conjunction with sending WorldState.
+pub const UniqueUser = struct {
+    id: u8,
+    user: User,
+};
+
+/// Used to send and receive the state of the program when a user enters a room.
+pub const WorldState = struct {
+    users: []UniqueUser,
+    image: []const u8,
+    // TODO
+    // image_size: Dot,
+    // layers: u8,
+};
