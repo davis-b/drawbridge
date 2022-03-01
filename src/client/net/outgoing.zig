84a0675534e35ed19f4b37a7dd7d80b9506eb3e3
Davis <obviouspy@gmail.com>
2022-03-01 05:58:58 -0500

    Clean up code

--- src/client/net/outgoing.zig
+++ src/client/net/outgoing.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 const ThreadContext = @import("index.zig").ThreadContext;
 const DrawAction = @import("actions.zig").Action;
-const packet = @import("packet.zig");
+const Packet = @import("net").Packet(.client);
 
 /// Something delivered from the main thread that is to be sent to the server.
 pub const OutgoingData = union(enum) {
@@ -11,22 +11,19 @@ pub const OutgoingData = union(enum) {
 };
 
 pub fn startSending(context: ThreadContext) void {
-    const client = context.client;
-    const pipe = context.pipe;
     while (true) {
-        const kind = pipe.out.wait(null) catch unreachable;
+        const msg = context.pipe.out.wait(null) catch unreachable;
         // TODO we probably have to serialize packet.WorldState and maybe DrawAction to a sequence of bytes that will be the same on every computer.
         // either packed extern struct or more manual serialization.
-        var packedMsg: packet.OutPacket = undefined;
-        switch (kind) {
-            .action => |action| {
-                packedMsg = packet.OutPacket{ .kind = .action, .data = std.mem.asBytes(&action) };
+        const packedMsg = switch (msg) {
+            .action => |*action| {
+                Packet{ .kind = .action, .data = std.mem.asBytes(action) };
             },
-            .state => |state| {
-                packedMsg = packet.OutPacket{ .kind = .state, .data = std.mem.asBytes(&state) };
+            .state => |*state| {
+                Packet{ .kind = .state, .data = std.mem.asBytes(state) };
             },
-        }
-        client.send(std.mem.asBytes(&packedMsg)) catch |err| {
+        };
+        context.client.send(std.mem.asBytes(&packedMsg)) catch |err| {
             pipe.meta.put(.net_exit) catch {
                 std.debug.print("Network write thread encountered a queue error while exiting.\n", .{});
             };
