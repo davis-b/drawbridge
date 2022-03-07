7601aa4b03b7ca0b45be82127b09d019e38763ef
Davis <obviouspy@gmail.com>
2022-03-07 02:24:33 -0500

    Refactor incoming-packet thread

--- src/client/net/incoming.zig
+++ src/client/net/incoming.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
 
+const net = @import("net");
+const cereal = @import("cereal");
+
 const ThreadContext = @import("index.zig").ThreadContext;
 const DrawAction = @import("actions.zig").Action;
 const packet = @import("packet.zig");
 const MetaEvent = @import("meta_events.zig").Event;
-
-const Packet = packet.Packet(.server);
 
 /// An incoming Action along with the associated user that spawned it.
 /// Ready for consumption by the main thread.
@@ -18,64 +19,64 @@ pub const PackagedAction = struct {
 /// It should not interact with the state of the world, as would be given when queried by a new client, to prevent desync issues.
 /// Primarily this thread will be decoding and placing data into the appropriate queue.
 pub fn startReceiving(context: ThreadContext) void {
-    const pipe = context.pipe;
-    const alloc = context.client.marshaller.allocator;
     var recv_buffer: [1024]u8 = undefined;
 
     while (true) {
         const dataPacket = context.client.recv(recv_buffer[0..]) catch {
-            pipe.meta.put(.net_exit) catch {
+            context.pipe.meta.put(.net_exit) catch {
                 std.debug.print("Network read thread encountered a queue error while exiting.\n", .{});
             };
             break;
         };
         // Our queue implementation copies the item we give it.
         // Thus, we do not have to worry about freeing the underlying memory before the queue consumer uses it.
-        defer alloc.free(dataPacket);
+        defer context.client.marshaller.allocator.free(dataPacket);
 
-        @compileLog("TODO: Deserialize this according to new packet semantics");
-        const message = std.mem.bytesToValue(packet.InPacket, dataPacket[0..@sizeOf(packet.InPacket)]);
-        switch (message.kind) {
-            .action => {
-                const action = std.mem.bytesToValue(DrawAction, message.data[0..@sizeOf(DrawAction)]);
-                pipe.in.put(PackagedAction{ .action = action, .userID = message.user }) catch {
-                    std.debug.print("Network read thread encountered a queue error adding to incoming action queue.\n", .{});
-                    break;
-                };
-            },
-            // A new peer has been detected. Either we joined a room, or they did.
-            // We must now share our current state with the server, which will be received as a peer_update packet.
-            .peer_entry => {
-                pipe.meta.put(MetaEvent{ .peer_entry = message.user }) catch {
-                    std.debug.print("Network read thread encountered a queue error while adding to meta queue [peer enter].\n", .{});
-                    break;
-                };
-            },
-            .peer_exit => {
-                pipe.meta.put(MetaEvent{ .peer_exit = message.user }) catch {
-                    std.debug.print("Network read thread encountered a queue error while adding to meta queue [peer exit].\n", .{});
-                    break;
-                };
-            },
-            // Server is querying this client for its current world state.
-            // A new peer has just connected, and will be caught up using this state.
-            .state_query => {
-                // The main thread will serialize and send world state.
-                // Signal only in meta queue, and then main thread puts it in network out queue, that should work.
-                // This way, we avoid a race condition where our state query response gets sent before an outgoing network action packet that was part of creating that state.
-                pipe.meta.put(MetaEvent{ .state_query = {} }) catch {
-                    std.debug.print("Network read thread encountered a queue error while adding to meta queue [state query].\n", .{});
-                    break;
-                };
-            },
-            // Set the state of this client.
-            .state_set => {
-                const world = std.mem.bytesToValue(packet.WorldState, @ptrCast(*const [@sizeOf(packet.WorldState)]u8, message.data));
-                pipe.meta.put(MetaEvent{ .state_set = world }) catch {
-                    std.debug.print("Network read thread encountered a queue error while adding to meta queue [state set].\n", .{});
-                    break;
-                };
-            },
-        }
+        const message = net.unwrap(.server, dataPacket) catch unreachable;
+        handleMsg(context, message) catch |err| {
+            std.debug.print("Network read thread encountered an error: {}.\n", .{err});
+            break;
+        };
+    }
+}
+
+fn handleMsg(context: ThreadContext, message: net.Packet(.server)) !void {
+    const pipe = context.pipe;
+    const err = MetaEvent{ .err = .unknown };
+    errdefer pipe.meta.put(err) catch {
+        std.debug.print("Meta pipe failed to put network-thread error\n", .{});
+    };
+    switch (message.kind) {
+        .action => {
+            const action = cereal.deserialize(null, PackagedAction, message.data) catch unreachable; // no allocation, can't fail.
+            try pipe.in.put(action);
+        },
+        // A new peer has been detected. They joined our room.
+        // We must now share our current state with the server, which will be received as a peer_update packet.
+        .peer_entry => {
+            try pipe.meta.put(MetaEvent{ .peer_entry = message.data[0] });
+        },
+        .peer_exit => {
+            try pipe.meta.put(MetaEvent{ .peer_exit = message.data[0] });
+        },
+        // Server is querying this client for its current world state.
+        // A new peer has just connected, and will be caught up using this state.
+        .state_query => {
+            // The main thread will serialize and send world state.
+            // Signal only in meta queue, and then main thread puts it in network out queue, that should work.
+            // This way, we avoid a race condition where our state query response gets sent before an outgoing network action packet that was part of creating that state.
+            try pipe.meta.put(MetaEvent{ .state_query = message.data[0] });
+        },
+        // Set the state of this client.
+        .state_set => {
+            // const world = std.mem.bytesToValue(packet.WorldState, @ptrCast(*const [@sizeOf(packet.WorldState)]u8, message.data));
+            const world = try cereal.deserialize(context.allocator, packet.WorldState, message.data);
+            try pipe.meta.put(MetaEvent{ .state_set = world });
+        },
+        .response => {
+            switch (try std.meta.intToEnum(net.FromServer.Response, message.data[0])) {
+                .room_full, .room_empty, .room_state_incoming, .try_again => return error.UnexpectedServerResponse,
+            }
+        },
     }
 }
