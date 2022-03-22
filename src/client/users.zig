4393b8411657b082b380bb7decfdfbde3a186386
Davis <obviouspy@gmail.com>
2022-03-22 13:23:44 -0400

    Remove dead code

--- src/client/users.zig
+++ src/client/users.zig
@@ -13,38 +13,3 @@ pub const User = packed struct {
 };
 
 pub const Peers = std.AutoHashMap(u8, User);
-// pub const Peers = struct {
-//     allocator: *std.mem.Allocator,
-//     map: std.AutoHashMap(u8, User),
-//
-//     pub fn init(allocator: *std.mem.Allocator) Peers {
-//         return Peers{
-//             .allocator = allocator,
-//             .map = std.AutoHashMap(u8, User).init(allocator),
-//         };
-//     }
-//
-//     pub fn deinit(self: *Peers) void {
-//         var it = self.map.iterator();
-//         for (it) |kv| {
-//             self.allocator.destroy(kv.value);
-//         }
-//         self.map.deinit();
-//     }
-//
-//     pub fn get(self: *Peers, id: u8) *User {
-//         return self.map.get(id) catch unreachable;
-//     }
-//
-//     pub fn remove(self: *Peers, id: u8) *User {
-//         //
-//     }
-//
-//     pub fn add(self: *Peers, id: u8, user: User) !void {
-//         var u = allocator.create(User);
-//         u.* = user;
-//         self.map.put(id, u);
-//     }
-// };
-
-// pub fn addUser(allocator: *std.mem.Allocator, id: u8, user: User, map: *Peers) !void {
